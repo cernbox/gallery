4f9fe5fa0e2920bec363ba950fb9e3cde3186b29
Hugo Gonzalez Labrador <github@hugo.labkode.com>
2017-02-07 11:06:28 +0100

    Hide Gallery menu entry and gallery button

--- appinfo/app.php
+++ appinfo/app.php
@@ -23,6 +23,7 @@ $appName = $c->query('AppName');
 /**
  * Menu entry in ownCloud
  */
+/*
 $c->query('OCP\INavigationManager')
   ->add(
 	  function () use ($c, $appName) {
@@ -50,6 +51,7 @@ $c->query('OCP\INavigationManager')
 		  ];
 	  }
   );
+*/
 
 /**
  * Loading translations
@@ -76,12 +78,12 @@ if (isset($request->server['REQUEST_URI'])) {
 		Util::addScript($appName, 'slideshow');
 		Util::addScript($appName, 'slideshowcontrols');
 		Util::addScript($appName, 'slideshowzoomablepreview');
-		Util::addScript($appName, 'gallerybutton');
+		//Util::addScript($appName, 'gallerybutton');
 
 		/**
 		 * Styles for the Files app
 		 */
 		Util::addStyle($appName, 'slideshow');
-		Util::addStyle($appName, 'gallerybutton');
+		//Util::addStyle($appName, 'gallerybutton');
 	}
 }// @codeCoverageIgnoreEnd
